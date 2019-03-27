f1ec906dc43f455440465c4865d49f7c39cca21f
Mithrandir0120 <mithrandir0120@gmail.com>
2019-03-27 18:55:00 +0800

    Connect to mysql

--- project_1622441019/settings.py
+++ project_1622441019/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'project_1622441019.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'smartcommunity_1622441019',
+        'USER': 'root',
+        'PASSWORD': 'HatsuneMiku',
+        'HOST': 'localhost',
+        'PORT': '3306',
     }
 }
 
